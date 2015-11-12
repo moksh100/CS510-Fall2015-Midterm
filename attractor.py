71d958910497b1a9e6790ed30dd61521df27d926
moksh100 <moksh100@mail.chapman.edu>
2015-11-11 20:55:57 -0800

    Update attractor.py

--- attractor.py
+++ attractor.py
@@ -13,6 +13,7 @@ class Attractor(object):
                 self.dt = (end-start)/points
 
         def euler (self, ar = np.array([]), dt=0):
+                """Calculate and return the result of the Euler Integration."""
                 if(dt==0):
                     dt =self.dt
                 x=ar[0]
@@ -27,6 +28,7 @@ class Attractor(object):
                 return np.array([xdt,ydt,zdt])
 
         def rk2 (self, ar = np.array([])):
+                 """Calculate and return the result of the Second Order Runge-Kutta Integration."""
                 dt=self.dt/2
                 k1= self.euler(ar)
                 x=ar[0]+k1[0]*dt
@@ -36,6 +38,7 @@ class Attractor(object):
                 return calc
 
         def rk3 (self, ar = np.array([])):
+                """Calculate and return the result of the Third Order Runge-Kutta Integration."""
                 dt=self.dt/2
                 k2= self.rk2(ar)
                 x=ar[0]+k2[0]*dt
@@ -45,6 +48,7 @@ class Attractor(object):
                 return calc
 
         def rk4 (self, ar = np.array([])):
+                 """Calculate and return the result of the 4th Order Runge-Kutta Integration."""
                 dt=self.dt
                 k3= self.rk3(ar)
                 x=ar[0]+k3[0]*dt
@@ -55,6 +59,7 @@ class Attractor(object):
             
             
         def evolve(self, r0= np.array([0.1, 0.0, 0.0]), order =4):
+            """Executed and Return the result of the selected method integration calculation."""
             iterator=np.arange(self.start+1, self.end, self.dt)
             iterator=np.append(iterator, self.end)
             result = np.array(np.append(0, r0))
@@ -83,36 +88,44 @@ class Attractor(object):
         
         
         def save(self):
+            """Export calculation to csv file."""
             self.solution.to_csv("export.csv")
             
         def plotx(self):
+            """Plot graph X."""
             self.solution['x'].plot()
             plt.show()
             
         def ploty(self):
+            """Plot graph Y."""
             self.solution['y'].plot()
             plt.show()
                         
         def plotz(self):
+            """Plot graph Z."""
             self.solution['z'].plot()
             plt.show()
             
         def plotxy(self):
+            """Plot graph X-Y."""
             plt.plot(self.solution['t'],self.solution['x'],'g')
             plt.plot(self.solution['t'],self.solution['y'],'r')
             plt.show()
         
         def plotyz(self):
+            """Plot graph Y-Z."""
             plt.plot(self.solution['t'],self.solution['y'],'g')
             plt.plot(self.solution['t'],self.solution['z'],'r')
             plt.show()
         
         def plotzx(self):
+            """Plot graph X-Z."""
             plt.plot(self.solution['t'],self.solution['z'],'g')
             plt.plot(self.solution['t'],self.solution['x'],'r')
             plt.show()
            
         def plot3d(self):
+            """Plot graph 3D."""
             fig = plt.figure()
             ax = fig.add_subplot(111, projection='3d')
             ax.scatter(self.solution['x'], self.solution['y'], self.solution['z'])
